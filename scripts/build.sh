3aa77842130319f5a8bf4776ede18d089e282234
wolfy <wolfy@shitwolfymakes.com>
2022-08-03 19:20:29 -0600

    Overhauled build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,74 +1,79 @@
-#!/bin/csh -f
+#!/bin/bash
+
+set -e
+RED='\033[1;31m'
+NC='\033[0m' # No Color
 
 # Instructions
-#  . make empty build directory
-#  . cp build.sh to empty build directory
-#  . Run ./build.sh
+#    . make empty build directory outside of the repo folder
+#    . cp build.sh to empty build directorys
+#    . Run ./build.sh
 
-set cdir = `pwd`
+# Install dependencies
+echo -e "${RED}Installing *nix dependency packages...${NC}"
+sudo apt install libpng-dev libjpeg-dev libtre-dev libfreetype6-dev qt5-default libqt5svg5-dev -y
 
-if (! -e CQPropertyView) then
-  git clone https://github.com/colinw7/CQPropertyView.git CQPropertyView
-endif
+cdir=`pwd`
 
-set inc = `cat CQPropertyView/src/CQPropertyView.pro | sed ':x; /\\$/ { N; s/\\\n//; tx }' | grep INCLUDEPATH | sed 's/INCLUDEPATH += //'`
+echo -e "${RED}Cloning CQPropertyView repo...${NC}"
+if [[ ! -d CQPropertyView ]]; then
+    git clone https://github.com/colinw7/CQPropertyView.git CQPropertyView
+fi
 
-set dirs = ()
+echo -e "${RED}Cloning dependencies...${NC}"
+include_paths=$(cat CQPropertyView/src/CQPropertyView.pro | sed ':x; /\\$/ { N; s/\\\n//; tx }' | grep INCLUDEPATH | sed 's/INCLUDEPATH += //')
+dependencies=()
+for path in $include_paths; do
+    n=$(echo $path | grep '..\/..\/' | wc -l)
+    if [[ $n != 1 ]]; then
+        continue
+    fi
 
-foreach v ($inc)
-  set n = `echo $v | grep '..\/..\/' | wc -l`
+    dep_name=$(echo $path | sed 's@../../\(.*\)/include@\1@')
+    dependencies+=($dep_name)
 
-  if ($n != 1) then
-    continue
-  endif
+    if [[ ! -d $dep_name ]]; then
+        echo -e "${RED}--- Clone $dep_name ---${NC}"
+        git clone https://github.com/colinw7/${dep_name}.git $dep_name
+        echo
+    fi
+done
 
-  set v1 = `echo $v | sed 's@../../\(.*\)/include@\1@'`
+# Build dependencies and project
+echo -e "${RED}Building dependencies...${NC}"
+for dep in ${dependencies[@]}; do
+    if [[ -d $cdir/$dep/src ]]; then
+        echo -e "${RED}--- Build $dep ---${NC}"
+        cd $cdir/$dep/src
 
-  set dirs = ($dirs $v1)
+        if [[ ! -d ../obj ]]; then
+            mkdir ../obj
+        fi
 
-  if (! -e $v1) then
-    echo "--- Clone $v1 ---"
+        if [[ ! -d ../lib ]]; then
+            mkdir ../lib
+        fi
 
-    git clone https://github.com/colinw7/${v1}.git $v1
-  endif
-end
+        n=$(find . -maxdepth 1 -name "*.pro" | wc -l)
+        if [[ $n == 1 ]]; then
+            qmake
+        fi
 
-foreach dir ($dirs)
-  echo "--- Build $dir ---"
+        make
+        echo
+    fi
+done
+echo -e "${RED}All dependencies built successfully!${NC}"
 
-  if (-e $cdir/$dir/src) then
-    cd $cdir/$dir/src
-
-    if (! -e ../obj) then
-      mkdir ../obj
-    endif
-
-    if (! -e ../lib) then
-      mkdir ../lib
-    endif
-
-    set n = `find . -maxdepth 1 -name "*.pro" | wc -l`
-
-    if ($n == 1) then
-      qmake
-    endif
-
-    make
-  endif
-end
-
-echo "--- Build CQPropertyView (src) ---"
-
+echo
+echo -e "${RED}--- Build CQPropertyView (src) ---${NC}"
 cd $cdir/CQPropertyView/src
-
 qmake
-
 make
 
-echo "--- Build CQPropertyView (test) ---"
-
+echo
+echo -e "${RED}--- Build CQPropertyView (test) ---${NC}"
 cd $cdir/CQPropertyView/test
-
 qmake
-
 make
+
