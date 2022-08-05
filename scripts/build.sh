42c353308ff56d5bbaa8e65798ac969c7cb4005c
wolfy <wolfy@shitwolfymakes.com>
2022-08-04 19:24:30 -0600

    build.sh now clones and builds each dependency to external/

--- scripts/build.sh
+++ scripts/build.sh
@@ -13,38 +13,35 @@ NC='\033[0m' # No Color
 echo -e "${RED}Installing *nix dependency packages...${NC}"
 sudo apt install libpng-dev libjpeg-dev libtre-dev libfreetype6-dev qt5-default libqt5svg5-dev -y
 
-cdir=`pwd`
-
-echo -e "${RED}Cloning CQPropertyView repo...${NC}"
-if [[ ! -d CQPropertyView ]]; then
-    git clone https://github.com/colinw7/CQPropertyView.git CQPropertyView
-fi
+git_root="$(git rev-parse --show-cdup)"
+git_root="${git_root:-./}"
+ext_dir="${git_root}external"
 
 echo -e "${RED}Cloning dependencies...${NC}"
-include_paths=$(cat CQPropertyView/src/CQPropertyView.pro | sed ':x; /\\$/ { N; s/\\\n//; tx }' | grep INCLUDEPATH | sed 's/INCLUDEPATH += //')
+include_paths=$(cat ${git_root}/src/CQPropertyView.pro | sed ':x; /\\$/ { N; s/\\\n//; tx }' | grep "INCLUDEPATH" | sed 's/INCLUDEPATH += //')
 dependencies=()
 for path in $include_paths; do
-    n=$(echo $path | grep '..\/..\/' | wc -l)
-    if [[ $n != 1 ]]; then
+    if ! $(echo "$path" | grep -q "..\/external\/"); then
         continue
     fi
+    
+    dep_name=$(echo "$path" | sed -r 's/\.\.\/external\/(.*)\/include/\1/')
+    dependencies+=("$dep_name")
 
-    dep_name=$(echo $path | sed 's@../../\(.*\)/include@\1@')
-    dependencies+=($dep_name)
-
-    if [[ ! -d $dep_name ]]; then
+    if [[ ! -d "$ext_dir/$dep_name" ]]; then
         echo -e "${RED}--- Clone $dep_name ---${NC}"
-        git clone https://github.com/colinw7/${dep_name}.git $dep_name
+        git clone "https://github.com/colinw7/${dep_name}.git" "$ext_dir/$dep_name"
         echo
     fi
 done
 
 # Build dependencies and project
 echo -e "${RED}Building dependencies...${NC}"
-for dep in ${dependencies[@]}; do
-    if [[ -d $cdir/$dep/src ]]; then
+working_dir=$(pwd)
+for dep in "${dependencies[@]}"; do
+    if [[ -d $ext_dir/$dep/src ]]; then
         echo -e "${RED}--- Build $dep ---${NC}"
-        cd $cdir/$dep/src
+        cd "$ext_dir/$dep/src"
 
         if [[ ! -d ../obj ]]; then
             mkdir ../obj
@@ -61,19 +58,23 @@ for dep in ${dependencies[@]}; do
 
         make
         echo
+        cd "$working_dir"
     fi
 done
 echo -e "${RED}All dependencies built successfully!${NC}"
-
 echo
 echo -e "${RED}--- Build CQPropertyView (src) ---${NC}"
-cd $cdir/CQPropertyView/src
+(
+cd "${git_root}src"
 qmake
 make
+)
 
 echo
 echo -e "${RED}--- Build CQPropertyView (test) ---${NC}"
-cd $cdir/CQPropertyView/test
+(
+cd "${git_root}test"
 qmake
 make
+)
 
