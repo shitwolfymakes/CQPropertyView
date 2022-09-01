7acde9237b940f3e1136c39d8d02b3e5c8c22070
wolfy <wolfy@shitwolfymakes.com>
2022-09-01 00:26:29 -0600

    stripped out basically everything unnecessary

--- tools/build.sh
+++ tools/build.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-
-set -e
+set -eo pipefail
 RED='\033[1;31m'
 NC='\033[0m' # No Color
 
@@ -8,9 +7,13 @@ NC='\033[0m' # No Color
 echo -e "${RED}Installing *nix dependency packages...${NC}"
 sudo apt install libpng-dev libjpeg-dev libtre-dev libfreetype6-dev qt5-default libqt5svg5-dev -y
 
-git_root="$(git rev-parse --show-cdup)"
-git_root="${git_root:-./}"
-ext_dir="${git_root}external"
+# cd into the directory this script is located
+cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null
+
+# Move into the repo root and use it as the working dir
+cd "$(git rev-parse --show-cdup)"
+git_root="$(pwd)"
+ext_dir="${git_root}/external"
 
 # Download dependencies
 echo -e "${RED}Cloning dependencies...${NC}"
@@ -18,35 +21,18 @@ dependencies=(CQUtil CConfig CFont CImageLib CMath CFile CFileUtil CStrUtil CReg
 for dep in "${dependencies[@]}"; do
     if [[ ! -d "$ext_dir/$dep" ]]; then
         echo -e "${RED}--- Clone $dep ---${NC}"
-        git clone "https://github.com/colinw7/${dep}.git" "$ext_dir/$dep"
+        git clone "git@github.com:colinw7/${dep}.git" "$ext_dir/$dep"
         echo
     fi
 done
 
 # Build dependencies and project
 echo -e "${RED}Building dependencies...${NC}"
-working_dir=$(pwd)
 for dep in "${dependencies[@]}"; do
-    if [[ -d $ext_dir/$dep/src ]]; then
+    if [[ -d $ext_dir/$dep ]]; then
         echo -e "${RED}--- Build $dep ---${NC}"
-        cd "$ext_dir/$dep/src"
-
-        if [[ ! -d ../obj ]]; then
-            mkdir ../obj
-        fi
-
-        if [[ ! -d ../lib ]]; then
-            mkdir ../lib
-        fi
-
-        n=$(find . -maxdepth 1 -name "*.pro" | wc -l)
-        if [[ $n == 1 ]]; then
-            qmake
-        fi
-
-        make
+        (cd "$ext_dir/$dep" && make)
         echo
-        cd "$working_dir"
     fi
 done
 echo -e "${RED}All dependencies built successfully!${NC}"
@@ -54,17 +40,9 @@ echo -e "${RED}All dependencies built successfully!${NC}"
 # Build CQPropertyView targets
 echo
 echo -e "${RED}--- Build CQPropertyView (src) ---${NC}"
-(
-cd "${git_root}src"
-qmake
-make
-)
+(cd "${git_root}/src" && qmake && make)
 
 echo
 echo -e "${RED}--- Build CQPropertyView (test) ---${NC}"
-(
-cd "${git_root}test"
-qmake
-make
-)
+(cd "${git_root}/test" && qmake && make)
 
