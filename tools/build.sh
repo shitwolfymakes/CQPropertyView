3ecbf6debcc18d9c00cc70b9097b2f22935a9885
wolfy <wolfy@shitwolfymakes.com>
2022-08-31 21:41:20 -0600

    script now uses hardcoded list of repo names

--- tools/build.sh
+++ tools/build.sh
@@ -4,11 +4,6 @@ set -e
 RED='\033[1;31m'
 NC='\033[0m' # No Color
 
-# Instructions
-#    . make empty build directory outside of the repo folder
-#    . cp build.sh to empty build directorys
-#    . Run ./build.sh
-
 # Install dependencies
 echo -e "${RED}Installing *nix dependency packages...${NC}"
 sudo apt install libpng-dev libjpeg-dev libtre-dev libfreetype6-dev qt5-default libqt5svg5-dev -y
@@ -17,20 +12,13 @@ git_root="$(git rev-parse --show-cdup)"
 git_root="${git_root:-./}"
 ext_dir="${git_root}external"
 
+# Download dependencies
 echo -e "${RED}Cloning dependencies...${NC}"
-include_paths=$(cat ${git_root}/src/CQPropertyView.pro | sed ':x; /\\$/ { N; s/\\\n//; tx }' | grep "INCLUDEPATH" | sed 's/INCLUDEPATH += //')
-dependencies=()
-for path in $include_paths; do
-    if ! $(echo "$path" | grep -q "..\/external\/"); then
-        continue
-    fi
-    
-    dep_name=$(echo "$path" | sed -r 's/\.\.\/external\/(.*)\/include/\1/')
-    dependencies+=("$dep_name")
-
-    if [[ ! -d "$ext_dir/$dep_name" ]]; then
-        echo -e "${RED}--- Clone $dep_name ---${NC}"
-        git clone "https://github.com/colinw7/${dep_name}.git" "$ext_dir/$dep_name"
+dependencies=(CQUtil CConfig CFont CImageLib CMath CFile CFileUtil CStrUtil CRegExp CGlob CUtil COS)
+for dep in "${dependencies[@]}"; do
+    if [[ ! -d "$ext_dir/$dep" ]]; then
+        echo -e "${RED}--- Clone $dep ---${NC}"
+        git clone "https://github.com/colinw7/${dep}.git" "$ext_dir/$dep"
         echo
     fi
 done
@@ -62,6 +50,8 @@ for dep in "${dependencies[@]}"; do
     fi
 done
 echo -e "${RED}All dependencies built successfully!${NC}"
+
+# Build CQPropertyView targets
 echo
 echo -e "${RED}--- Build CQPropertyView (src) ---${NC}"
 (
